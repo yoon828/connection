865da33543abe0eaf70ed0fedf0156bf2c2aaa7c
kian6365 <kian6365@naver.com>
2022-10-18 17:35:53 +0900

    [DATA] 백준 전체문제 태그 포함 저장

--- backend/connection/src/main/java/com/ssafy/connection/dto/TagDto.java
+++ backend/connection/src/main/java/com/ssafy/connection/dto/TagDto.java
@@ -2,13 +2,21 @@ package com.ssafy.connection.dto;
 
 import com.ssafy.connection.entity.Tag;
 import com.ssafy.connection.util.ModelMapperUtils;
+import lombok.*;
 
+@Data
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TagDto {
     private long tagId;
 
-    private String key;  // 영어 이름
+    private String en;  // 영어 이름
 
-    private String name; // 한글 이름
+    private String ko; // 한글 이름
 
     public static TagDto of(Tag tagEntity) {
         TagDto tagDto = ModelMapperUtils.getModelMapper().map(tagEntity, TagDto.class);
