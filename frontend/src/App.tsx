a3a3da6374b02efb738b3c39b4c75914bccc96c0
김윤민 <k915k1004@naver.com>
2022-10-24 11:55:47 +0900

    Revert "Merge branch 'FE_A_김윤민' into 'develop'"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
 import React from "react";
-import Routes from "./pages/Routes";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <Routes />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
