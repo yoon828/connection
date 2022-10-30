e433daa1581fee80cf12af80dc9a876d0917a8d1
dnrjs8185 <dnrjs8185@naver.com>
2022-10-30 20:27:00 +0900

    [S07P31C202-86] organization 초대

---
+++ backend/connection/src/main/java/com/ssafy/connection/dto/GithubUserDetailsDto.java
@@ -0,0 +1,15 @@
+package com.ssafy.connection.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class GithubUserDetailsDto {
+
+    private String login;
+
+    private long id;
+}
