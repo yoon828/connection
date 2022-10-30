239f65f501f4654f87611bfc4f9812951f28ae00
dnrjs8185 <dnrjs8185@naver.com>
2022-10-30 16:20:12 +0900

    [S07P31C202-60] 스터디 생성&참가&탈퇴/추방&해체 예외처리

--- backend/connection/src/main/java/com/ssafy/connection/repository/ConnStudyRepository.java
+++ backend/connection/src/main/java/com/ssafy/connection/repository/ConnStudyRepository.java
@@ -8,8 +8,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ConnStudyRepository extends JpaRepository<ConnStudy, Long> {
-    Optional<ConnStudy> findByStudy_StudyId(long studyId);
+    Optional<ConnStudy> findByStudy_StudyIdAndRole(long studyId, String role);
     Optional<ConnStudy> findByUser_UserId(long userId);
     ConnStudy findByUser(User userEntity);
     List<ConnStudy> findAllByStudy_StudyId(long studyId);
+    Optional<ConnStudy> findByUser_UserIdAndStudy_StudyId(long userId, long studyId);
+    Optional<ConnStudy> findByUser_UserIdAndRole(long studyId, String role);
 }
