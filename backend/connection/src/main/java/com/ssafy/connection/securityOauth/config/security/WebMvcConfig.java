58437dbd448f0f242bd135ea6eb875e93334ab4c
kian6365 <kian6365@naver.com>
2022-10-31 13:40:51 +0900

    [fix] cors 수정

--- backend/connection/src/main/java/com/ssafy/connection/securityOauth/config/security/WebMvcConfig.java
+++ backend/connection/src/main/java/com/ssafy/connection/securityOauth/config/security/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
