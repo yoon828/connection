b3b1da16d81e75bd5061a5475eaa8df45d2eee8f
kian6365 <kian6365@naver.com>
2022-11-01 14:04:11 +0900

    [etc] 문제 검색 수정

--- backend/connection/src/main/java/com/ssafy/connection/service/SubjectService.java
+++ backend/connection/src/main/java/com/ssafy/connection/service/SubjectService.java
@@ -5,5 +5,5 @@ import com.ssafy.connection.entity.Subject;
 
 public interface SubjectService {
     int makeSubject(SubjectDto subjectDto, Long userId);
-    void getTeamStatus(Long studyId, Long userId);
+    void getTeamStatus(Long userId);
 }
--- backend/connection/src/main/java/com/ssafy/connection/service/SubjectServiceImpl.java
+++ backend/connection/src/main/java/com/ssafy/connection/service/SubjectServiceImpl.java
@@ -1,10 +1,15 @@
 package com.ssafy.connection.service;
 
 import com.ssafy.connection.dto.SubjectDto;
+import com.ssafy.connection.entity.ConnStudy;
+import com.ssafy.connection.entity.Study;
 import com.ssafy.connection.entity.Subject;
+import com.ssafy.connection.repository.ConnStudyRepository;
 import com.ssafy.connection.repository.ProblemRepository;
 import com.ssafy.connection.repository.StudyRepository;
 import com.ssafy.connection.repository.SubjectRepository;
+import com.ssafy.connection.securityOauth.domain.entity.user.User;
+import com.ssafy.connection.securityOauth.repository.user.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,11 +21,16 @@ public class SubjectServiceImpl implements SubjectService{
     private final SubjectRepository subjectRepository;
     private final StudyRepository studyRepository;
     private final ProblemRepository problemRepository;
+    private final ConnStudyRepository  connStudyRepository;
+    private final UserRepository userRepository;
 
-    public SubjectServiceImpl(SubjectRepository subjectRepository, StudyRepository studyRepository, ProblemRepository problemRepository){
+    public SubjectServiceImpl(SubjectRepository subjectRepository, StudyRepository studyRepository, ProblemRepository problemRepository,
+                              ConnStudyRepository connStudyRepository, UserRepository userRepository){
         this.subjectRepository = subjectRepository;
         this.studyRepository = studyRepository;
         this.problemRepository = problemRepository;
+        this.connStudyRepository = connStudyRepository;
+        this.userRepository = userRepository;
     }
 
 //    @Override
@@ -46,7 +56,11 @@ public class SubjectServiceImpl implements SubjectService{
     }
 
     @Override
-    public void getTeamStatus(Long studyId, Long userId){
-
+    public void getTeamStatus(Long userId){
+        Study studyEntity = studyRepository.findByConnStudy(
+                                connStudyRepository.findByUser(
+                                    userRepository.findById(userId).get()));
+        List<Subject> subjectList = subjectRepository.findAllByStudy(studyEntity);
+        System.out.println(subjectList.toString());
     }
 }
