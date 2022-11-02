38aee978044a4d1958c38c786c4aec5dd87f7490
kian6365 <kian6365@naver.com>
2022-11-02 11:32:14 +0900

    [etc] change response

--- backend/connection/src/main/java/com/ssafy/connection/controller/WorkbookController.java
+++ backend/connection/src/main/java/com/ssafy/connection/controller/WorkbookController.java
@@ -1,6 +1,7 @@
 package com.ssafy.connection.controller;
 
 import com.ssafy.connection.dto.ProblemReturnDto;
+import com.ssafy.connection.dto.ResponseDto;
 import com.ssafy.connection.securityOauth.config.security.token.CurrentUser;
 import com.ssafy.connection.securityOauth.config.security.token.UserPrincipal;
 import com.ssafy.connection.service.ProblemService;
@@ -12,7 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/workbook")
@@ -30,58 +33,48 @@ public class WorkbookController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "problemId", value = "추가할 문제 번호", required = true)
     })
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "성공"),
-            @ApiResponse(code = 400, message = "실패(이미 문제집에 해당 문제가 있음)"),
-            @ApiResponse(code = 404, message = "실패(번호에 해당하는 문제가 없음)")
-    })
     @PostMapping("/")
-    public ResponseEntity<String> addProblem(@RequestParam("problemId") Long problemId, @Parameter(description = "Accesstoken", required = true) @CurrentUser UserPrincipal userPrincipal){
+    public ResponseEntity<ResponseDto> addProblem(@RequestParam("problemId") Long problemId, @Parameter(description = "Accesstoken", required = true) @CurrentUser UserPrincipal userPrincipal){
         switch(workbookService.addProblem(problemId, userPrincipal.getId())){
             case 1:
-                return ResponseEntity.status(HttpStatus.NO_CONTENT).body("success");
+                return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("success"));
             case 0:
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("already exist");
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseDto("already exist"));
             case 2:
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("wrong problemId");
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseDto("wrong parameter value"));
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("fail");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseDto("fail"));
     }
 
     @ApiOperation(value = "문제 삭제", notes = "현재 유저가 속해있는 스터디의 문제집에서 문제 삭제")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "problemId", value = "삭제할 문제 번호", required = true)
     })
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "성공"),
-            @ApiResponse(code = 400, message = "실패(번호에 해당하는 문제가 없음)"),
-            @ApiResponse(code = 404, message = "실패(문제집에 해당 문제가 없음)")
-    })
     @DeleteMapping("/")
-    public ResponseEntity<String> deleteProblem(@RequestParam("problemId") Long problemId, @Parameter(description = "Accesstoken", required = true) @CurrentUser UserPrincipal userPrincipal){
+    public ResponseEntity<ResponseDto> deleteProblem(@RequestParam("problemId") Long problemId, @Parameter(description = "Accesstoken", required = true) @CurrentUser UserPrincipal userPrincipal){
         switch(workbookService.deleteProblem(problemId, userPrincipal.getId())){
             case 1:
-                return ResponseEntity.status(HttpStatus.OK).body("success");
+                return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto("success"));
             case 0:
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("already deleted");
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseDto("already delete"));
             case -1:
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("wrong problemId");
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseDto("wrong parameter value"));
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("fail");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseDto("fail"));
     }
 
     @ApiOperation(value = "문제 조회", notes = "현재 유저가 속해있는 스터디의 문제집에서 문제 리스트 조회")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "성공"),
-            @ApiResponse(code = 404, message = "실패(문제집이 비었음)")
-    })
     @GetMapping("/")
-    public ResponseEntity<List<ProblemReturnDto>> getProblem(@Parameter(description = "Accesstoken", required = true) @CurrentUser UserPrincipal userPrincipal){
+    public ResponseEntity<Map<String, Object>> getProblem(@Parameter(description = "Accesstoken", required = true) @CurrentUser UserPrincipal userPrincipal){
+        Map<String, Object> returnMap = new HashMap<>();
         List<ProblemReturnDto> list = workbookService.getProblem(userPrincipal.getId());
+        returnMap.put("list", list);
         if(list.size() > 0){
-            return ResponseEntity.status(HttpStatus.OK).body(list);
+            returnMap.put("msg", "success");
+            return ResponseEntity.status(HttpStatus.OK).body(returnMap);
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(list);
+            returnMap.put("msg", "empty");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(returnMap);
         }
     }
 
