46fcf3fa7959898c4cc19a36069c2b175747e823
최진합 <pppooo95@naver.com>
2022-11-04 13:34:50 +0900

    refactor - ProblemList 컴포넌트 분리

---
+++ frontend/src/components/recommend/ProblemList.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import { Grid, useToast } from "@chakra-ui/react";
+
+import { addWorkbook, deleteWorkbook, getWorkbook } from "../../api/workbook";
+import { Problem } from "../../pages/Recommend";
+import { useAppSelector } from "../../store/hooks";
+import ProblemCard from "../common/ProblemCard";
+
+interface ProblemListProps {
+  problemList: Problem[];
+}
+
+function ProblemList({ problemList }: ProblemListProps) {
+  const auth = useAppSelector(state => state.auth);
+  const [myWorkbook, setMyWorkbook] = useState<Problem[]>([]);
+  const [btnTypes, setBtnTypes] = useState(
+    problemList.map(problem =>
+      myWorkbook.findIndex(
+        p => p.problemInfo.problemId === problem.problemInfo.problemId
+      ) >= 0
+        ? "delete"
+        : "add"
+    )
+  );
+  const toast = useToast();
+
+  const addProblem = async (problemId: number, idx: number) => {
+    const res = await addWorkbook(problemId);
+    const newBtnTypes = [...btnTypes];
+    newBtnTypes[idx] = "delete";
+    setBtnTypes(newBtnTypes);
+    toast({
+      title: `${problemId}번 문제를 추가했습니다`,
+      position: "top",
+      isClosable: true
+    });
+    console.log(problemId);
+  };
+  const deleteProblem = async (problemId: number, idx: number) => {
+    const res = await deleteWorkbook(problemId);
+    const newBtnTypes = [...btnTypes];
+    newBtnTypes[idx] = "add";
+    setBtnTypes(newBtnTypes);
+    toast({
+      title: `${problemId}번 문제를 삭제했습니다!`,
+      status: "warning",
+      position: "top",
+      isClosable: true
+    });
+    console.log(problemId);
+  };
+  useEffect(() => {
+    setBtnTypes(
+      problemList.map(problem =>
+        myWorkbook.findIndex(
+          p => p.problemInfo.problemId === problem.problemInfo.problemId
+        ) >= 0
+          ? "delete"
+          : "add"
+      )
+    );
+  }, [problemList]);
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await getWorkbook();
+      setMyWorkbook(res.data);
+    };
+    fetch();
+  }, []);
+
+  return (
+    <Grid templateColumns="repeat(2,1fr)" gap="32px">
+      {problemList.map((problem, idx) => (
+        <ProblemCard
+          key={problem.problemInfo.problemId}
+          problem={problem}
+          btnType={btnTypes[idx]}
+          hasBtn={auth.information.studyId !== 0}
+          onBtnClick={() => {
+            if (btnTypes[idx] === "add")
+              addProblem(problem.problemInfo.problemId, idx);
+            else deleteProblem(problem.problemInfo.problemId, idx);
+          }}
+        />
+      ))}
+    </Grid>
+  );
+}
+
+export default ProblemList;
