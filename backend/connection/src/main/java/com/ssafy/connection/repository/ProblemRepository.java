b2c961d3f5eb82fa837b60ea1b687fd073203316
kian6365 <kian6365@naver.com>
2022-11-07 17:05:24 +0900

    [S07P31C202-98] fix : 문제 검색 속도 개선

--- backend/connection/src/main/java/com/ssafy/connection/repository/ProblemRepository.java
+++ backend/connection/src/main/java/com/ssafy/connection/repository/ProblemRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.connection.repository;
 
+import com.ssafy.connection.dto.ProblemSearchInterface;
 import com.ssafy.connection.entity.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -21,4 +22,7 @@ public interface ProblemRepository extends JpaRepository<Problem, Long> {
 
     @Query(value = "SELECT * FROM problem p JOIN tag t ON p.problem_id = t.problem_id WHERE p.accepted > 10000 AND p.level > 2 AND t.ko = :tag", nativeQuery = true)
     List<Problem> findPopularProblemListByTag(String tag);
+
+    @Query(value = "select p.problem_id problemId, p.title, p.accepted, p.level, p.tries, t.tag_id tagId, t.en, t.ko FROM problem p JOIN tag t ON p.problem_id = t.problem_id WHERE upper(title) like %:title%", nativeQuery = true)
+    List<ProblemSearchInterface> findAllByTitle2(String title);
 }
