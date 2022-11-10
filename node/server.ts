ea61bd1e58bb25dbbc91f3d67e2bb8d88f226ed1
염진호 <pwlsghq@naver.com>
2022-11-10 09:36:03 +0900

    fix : node stack overflow

--- node/server.ts
+++ node/server.ts
@@ -101,13 +101,13 @@ const getSolvingInfo = (
 };
 
 const getStudyInfo = (studyId: string): StudyInfoType => {
-  const studyInfo = getStudyInfo(studyId);
+  const studyInfo = studyInfos.get(studyId);
   if (studyInfo) return studyInfo;
   throw new Error(`ID : ${studyId} 가 존재하지 않습니다.`);
 };
 
 const getUserInfo = (bojId: string): UserInfoType => {
-  const userInfo = getUserInfo(bojId);
+  const userInfo = userInfos.get(bojId);
   if (userInfo) return userInfo;
   throw new Error(`BOJ_ID : ${bojId} 가 존재하지 않습니다.`);
 };
