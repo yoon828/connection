30ea7112ee38e58340ba831d55366133b7d0d846
yoon <yoon828990@gmail.com>
2023-04-14 21:32:01 +0900

    route code split

--- frontend/src/pages/routes/Routes.tsx
+++ frontend/src/pages/routes/Routes.tsx
@@ -4,19 +4,20 @@ import {
   Route,
   Routes as ReactRouterRoutes
 } from "react-router-dom";
-
-import Recommend from "../Recommend";
-import StudyJoin from "../StudyJoin";
-import Header from "../Header";
-import StudyWith from "../StudyWith";
-import Collection from "../study/Collection.tsx";
-import StudyTotal from "../study/StudyTotal";
-import Assignment from "../study/Assignment";
-import Management from "../study/Management";
-import Main from "../Main";
-import Callback from "../Callback";
 import { useAppSelector } from "../../store/hooks";
 import PrivateRoute, { PrivateRouteProps } from "./PrivateRoute";
+import Main from "../Main";
+
+const Recommend = React.lazy(() => import("../Recommend"));
+const StudyJoin = React.lazy(() => import("../StudyJoin"));
+const Header = React.lazy(() => import("../Header"));
+const StudyWith = React.lazy(() => import("../StudyWith"));
+const Collection = React.lazy(() => import("../study/Collection.tsx"));
+const StudyTotal = React.lazy(() => import("../study/StudyTotal"));
+const Assignment = React.lazy(() => import("../study/Assignment"));
+const Management = React.lazy(() => import("../study/Management"));
+// const Main = React.lazy(() => import("../Main"));
+const Callback = React.lazy(() => import("../Callback"));
 
 export default function Routes() {
   const auth = useAppSelector(state => state.auth);
@@ -30,17 +31,6 @@ export default function Routes() {
       <Header />
       <ReactRouterRoutes>
         <Route path="/" element={<Main />} />
-        {/* <Route path="/recommend" element={<Recommend />} />
-        <Route path="/study">
-          <Route index element={<StudyTotal />} />
-          <Route path="join" element={<StudyJoin />} />
-          <Route path="collection" element={<Collection />} />
-          <Route path="assignment" element={<Assignment />} />
-          <Route path="management" element={<Management />} />
-        </Route>
-        <Route path="/study-with" element={<StudyWith />} />
-        <Route path="/oauth2/redirect" element={<Callback />} /> */}
-
         <Route
           path="/recommend"
           element={
